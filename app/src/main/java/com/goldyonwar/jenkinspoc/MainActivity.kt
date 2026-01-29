78bddd9721d80b03d2dbc3e59a90167dd2929fc3
Gold Yon War <goldyonwar@gmail.com>
2026-01-29 21:50:28 +1300

    Android build 11

--- app/src/main/java/com/goldyonwar/jenkinspoc/MainActivity.kt
+++ app/src/main/java/com/goldyonwar/jenkinspoc/MainActivity.kt
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     JenkinsPocTheme {
-        Greeting("Android")
+        Greeting("Android build 11")
     }
 }
